fd0378f528ef1606fd89acfe4981da5fd1afd887
GeniusCorn <hi@nicecorn.com>
2022-12-04 15:59:21 +0800

    refactor: dashboard navbar

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import 'uno.css'
-import 'primevue/resources/themes/saga-blue/theme.css'
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
 
 import App from './App.vue'
-import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import router from '@/router/'
 
 const app = createApp(App)
 
-app.use(router).use(PrimeVue).mount('#app')
+app.use(router).mount('#app')
