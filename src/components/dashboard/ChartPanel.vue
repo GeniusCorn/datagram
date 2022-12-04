a868113929438aeef45dc4759ee25cb313e7e44b
GeniusCorn <hi@nicecorn.com>
2022-12-04 18:00:23 +0800

    feat: dashboard left bar

---
+++ src/components/dashboard/ChartPanel.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { useMessage } from 'naive-ui'
+
+const message = useMessage()
+
+const emit = defineEmits<{
+  (e: 'submit', chart: string): void
+}>()
+
+let selectedChart: string | undefined = $ref(undefined)
+
+function selectChart(chart: string) {
+  selectedChart = chart
+}
+
+function submitSelectedChart(chart: string | undefined) {
+  if (chart === undefined) {
+    message.error('请选择一个图表类型')
+    return
+  }
+
+  message.info(`选择${chart}`)
+  selectedChart = undefined
+  emit('submit', chart)
+}
+</script>
+
+<template>
+  <n-modal w-600px preset="card" title="添加视图">
+    <div flex="~" justify-around>
+      <div
+        class="chartBlock"
+        :class="{ selected: selectedChart === 'basicBarPlot' }"
+        @click="selectChart('basicBarPlot')"
+      >
+        柱状图
+      </div>
+      <div
+        class="chartBlock"
+        :class="{ selected: selectedChart === 'basicPiePlot' }"
+        @click="selectChart('basicPiePlot')"
+      >
+        饼图
+      </div>
+    </div>
+
+    <template #footer>
+      <n-button
+        float-right
+        type="primary"
+        @click="submitSelectedChart(selectedChart)"
+        >确认</n-button
+      >
+    </template>
+  </n-modal>
+</template>
+
+<style scoped>
+.selected {
+  @apply border-[#18a058] text-[#18a058];
+}
+
+.chartBlock {
+  @apply h-16 w-16 border rounded cursor-pointer flex flex-col items-center;
+}
+</style>
---
+++ src/components/dashboard/DashBoardLeftBar.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { Icon } from '@vicons/utils'
+import { ChartPie, Map2, Components } from '@vicons/tabler'
+
+let showChartPanel: boolean = $ref(false)
+
+let selectedChart: string | undefined = $ref(undefined)
+
+function onSubmitChart(chart: string) {
+  showChartPanel = false
+  selectedChart = chart
+}
+</script>
+
+<template>
+  <div flex="~ col" bg="[#f8f9fa]" w-20 h-full items-center gap-4>
+    <div
+      flex="~ col"
+      gap-1
+      items-center
+      hover:text-gray
+      cursor-pointer
+      mt-24px
+      @click="showChartPanel = true"
+    >
+      <Icon size="25">
+        <ChartPie />
+      </Icon>
+      <div>视图</div>
+    </div>
+
+    <ChartPanel v-model:show="showChartPanel" @submit="onSubmitChart" />
+
+    <n-divider />
+
+    <div flex="~ col" gap-1 items-center hover:text-gray cursor-pointer>
+      <Icon size="25">
+        <Components />
+      </Icon>
+      <div>组件</div>
+    </div>
+
+    <n-divider />
+
+    <div flex="~ col" gap-1 items-center hover:text-gray cursor-pointer>
+      <Icon size="25">
+        <Map2 />
+      </Icon>
+      <div>地图</div>
+    </div>
+
+    <n-divider />
+  </div>
+</template>
+
+<style scoped></style>
